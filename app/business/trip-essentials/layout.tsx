8fb0e1b3652312a2acb9b86fdcee73138623bbc1
Yuvaraj S <yuvarajs@Yuvarajs-MacBook-Air.local>
2026-01-05 17:44:11 +0530

    added headings

--- app/business/trip-essentials/layout.tsx
+++ app/business/trip-essentials/layout.tsx
@@ -1,14 +1,5 @@
 import type { Metadata } from "next";
-import { Lato } from "next/font/google";
 import Header from "@/components/business/common/Header";
-
-
-
-const lato = Lato({
-  subsets: ["latin"],
-  weight: ["300", "400", "700", "900"],
-  variable: "--font-lato",
-});
 
 
 export const metadata: Metadata = {
